385eab13a6a147eda11dd4087e36be7226bd3d93
Ben Timby <btimby@gmail.com>
2012-02-06 13:09:43 -0500

    Now using mimetypes, simply cat unrecognized file types.

--- fulltext/__init__.py
+++ fulltext/__init__.py
@@ -1,31 +1,27 @@
-import os, os.path, subprocess, re, csv, time, select
-# TODO: support plain text files. Just read their content.
+import os, os.path, subprocess, re, csv, time, select, mimetypes
 # TODO: support file-like objects (pipe file data to stdin).
-# TODO: use mimetypes library for figuring out file type.
+
+mimetypes.add_type('application/rar', '.rar')
 
 PROG_MAP = {
-    '.pdf':     ('/usr/bin/pdftotext', '-q', '-nopgbrk', '{0}', '-'),
-    '.doc':     ('/usr/bin/antiword', '{0}'),
-    '.docx':    ('/usr/local/bin/docx2txt', '{0}', '-'),  # http://sourceforge.net/projects/docx2txt/
-    #'.xls':     '/usr/bin/xls2csv {0}',
-    '.xls':     ('/usr/bin/convertxls2csv', '-q', '-x {0}', '-c -'),
-    '.rtf':     ('/usr/bin/unrtf', '--text', '--nopict', '{0}'),
-    '.odt':     ('/usr/bin/odt2txt', '{0}'),
-    '.ods':     ('/usr/bin/odt2txt', '{0}'),
-    '.zip':     ('/usr/bin/zipinfo', '-2z', '{0}'),
-    '.tar.gz':  ('/bin/tar', 'tzf', '{0}'),
-    '.tar.bz2': ('/bin/tar', 'tjf', '{0}'),
-    '.rar':     ('/usr/bin/unrar', 'vb', '-p-', '-ierr', '-y', '{0}'),
-    '.htm':     ('/usr/bin/html2text', '-nobs', '{0}'),
-    '.html':    ('/usr/bin/html2text', '-nobs', '{0}'),
-    '.xml':     ('/usr/bin/html2text', '-nobs', '{0}'),
-    '.jpg':     ('/usr/bin/exiftool', '-s', '-s', '-s', '{0}'),
-    '.jpeg':    ('/usr/bin/exiftool', '-s', '-s', '-s', '{0}'),
-    '.mpg':     ('/usr/bin/exiftool', '-s', '-s', '-s', '{0}'),
-    '.mpeg':    ('/usr/bin/exiftool', '-s', '-s', '-s', '{0}'),
-    '.mp3':     ('/usr/bin/exiftool', '-s', '-s', '-s', '{0}'),
-    '.dll':     ('/usr/bin/strings', '-n 18', '{0}'),
-    '.exe':     ('/usr/bin/strings', '-n 18', '{0}'),
+    (None, None): ('cat', '{0}'),
+    ('application/pdf', None): ('pdftotext', '-q', '-nopgbrk', '{0}', '-'),
+    ('application/msword', None): ('antiword', '{0}'),
+    ('application/vnd.openxmlformats-officedocument.wordprocessingml.document', None): ('docx2txt', '{0}', '-'),  # http://sourceforge.net/projects/docx2txt/
+    ('application/vnd.ms-excel', None): ('convertxls2csv', '-q', '-x {0}', '-c -'),
+    ('application/rtf', None): ('unrtf', '--text', '--nopict', '{0}'),
+    ('application/vnd.oasis.opendocument.text', None): ('odt2txt', '{0}'),
+    ('application/vnd.oasis.opendocument.spreadsheet', None): ('odt2txt', '{0}'),
+    ('application/zip', None): ('zipinfo', '-2z', '{0}'),
+    ('application/x-tar', 'gzip'): ('tar', 'tzf', '{0}'),
+    ('application/x-tar', 'gzip2'): ('tar', 'tjf', '{0}'),
+    ('application/rar', None): ('unrar', 'vb', '-p-', '-ierr', '-y', '{0}'),
+    ('text/html', None): ('html2text', '-nobs', '{0}'),
+    ('text/xml', None): ('html2text', '-nobs', '{0}'),
+    ('image/jpeg', None): ('exiftool', '-s', '-s', '-s', '{0}'),
+    ('video/mpeg', None): ('exiftool', '-s', '-s', '-s', '{0}'),
+    ('audio/mpeg', None): ('exiftool', '-s', '-s', '-s', '{0}'),
+    ('application/octet-stream', None): ('strings', '-n 18', '{0}'),
 }
 
 STRIP_WHITE = re.compile(r'[ \t\v\f\r\n]+')
@@ -51,20 +47,39 @@ def csv_to_text(text):
     return ' '.join(buffer)
 
 FUNC_MAP = {
-    '.rtf': strip_unrtf_header,
-    '.xls': csv_to_text,
+    ('application/rtf', None): strip_unrtf_header,
+    ('application/vnd.ms-excel', None): csv_to_text,
 }
 
 class FullTextException(Exception):
     pass
 
 
+# From:
+# http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
+
+def add_type(mime, command, encoding=None):
+    assert isinstance(command, tuple), 'Command must be tuple.'
+    assert '{0}' in command, 'Command must contain {0}.'
+    PROG_MAP[(mime, encoding)] = command
+
+
 def get_type(filename):
-    name, ext = os.path.splitext(filename)
-    if ext in ('.gz', '.bz2') and name.endswith('.tar'):
-        ext = '.tar' + ext
-    if ext in PROG_MAP:
-        return ext
+    return mimetypes.guess_type(filename)
 
 
 def get(filename, default=None, type=None):
@@ -83,12 +98,10 @@ def get(filename, default=None, type=None):
             return default
         raise FullTextException('Unknown file type, known file types are: {0}'.format(' '.join(PROG_MAP.keys())))
     cmd = map(lambda x: x.format(filename), cmd)
-    if not os.access(cmd[0], os.F_OK | os.X_OK):
-        if default is not None:
-            return default
+    if which(cmd[0]) is None:
         raise FullTextException('Cannot execute binary: {0}'.format(cmd[0]))
     try:
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         f = p.stdout.fileno()
         s, b = time.time(), []
         while True:
@@ -113,7 +126,9 @@ def get(filename, default=None, type=None):
 
 
 def check():
+    commands = {}
     for type, cmd in PROG_MAP.items():
-        if not os.access(cmd[0], os.F_OK | os.X_OK):
-            print 'Cannot find converter for {0}, please install: {1}'.format(type, cmd[0])
-    print 'All other converters present and accounted for.'
+        commands[cmd[0]] = None
+    for cmd in commands.keys():
+        if which(cmd) is None:
+            print 'Cannot execute command {0}, please install it.'.format(cmd)
