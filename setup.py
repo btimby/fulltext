1cf0dc248699d79d8c65f042ef526d88922e9102
Ben Timby <btimby@gmail.com>
2013-02-15 11:03:18 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import os
 from distutils.core import setup
 
 name = 'fulltext'
-version = '0.3'
+version = '0.4'
 release = '1'
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 versrel = version + '-' + release
-download_url = 'https://github.com/downloads/btimby/' + name + \
-                           '/' + name + '-' + versrel + '.tar.gz'
 long_description = file(readme).read()
 
 setup(
@@ -22,7 +20,6 @@ setup(
     maintainer = 'Ben Timby',
     maintainer_email = 'btimby@gmail.com',
     url = 'http://github.com/btimby/' + name + '/',
-    download_url = download_url,
     license = 'GPLv3',
     packages = ["fulltext"],
     classifiers = (
