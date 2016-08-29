a8dabb252104db341d108a779a6596630d95ab0b
Jordan Reiter <jordan@aace.org>
2016-08-29 16:39:32 -0400

    Add tests for files with UTF characters, and use context managers to open files

--- tests.py
+++ tests.py
@@ -14,6 +14,15 @@ TEST = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ipsum augu
 "tortor. Ut a justo non dolor venenatis accumsan. Proin dolor eros, aliquam id condimentum et, aliquam quis metus. " \
 "Vivamus eget purus diam."
 
+ENC_TEST = u"Lorem ipsum dolor sit amet, cons\xe9ct\xeatur adipiscing elit. Nunc ipsum augue, iaculis quis " \
+u"auctor eu, adipiscing non est. Nullam id sem diam, \xe9g\xeat varius dui. Etiam sollicitudin sapien nec odio " \
+u"\xe9lem\xeantum sit amet luctus magna volutpat. Ut commodo nulla n\xe9qu\xea. Aliquam erat volutpat. " \
+u"Int\xe9g\xear et nunc augue. P\xe9llent\xeasque habitant morbi tristique s\xe9n\xeactus et netus et malesuada " \
+u"fames ac turpis \xe9g\xeastas. Quisque at enim nulla, vel tincidunt urna. Nam leo augue, \xe9lem\xeantum ut " \
+u"viverra \xe9g\xeat, sc\xe9l\xearisque in purus. In arcu orci, porta nec aliquet quis, pretium a sem. In " \
+u"f\xe9rm\xeantum nisl id diam luctus viverra. Nullam s\xe9mp\xear, metus at euismod vulputate, orci odio " \
+u"dignissim urna, quis iaculis neque lacus ut tortor. Ut a justo non dolor v\xe9n\xeanatis accumsan. Proin dolor " \
+u"eros, aliquam id condimentum et, aliquam quis metus. Vivamus \xe9g\xeat purus diam."
 
 def allow_missing_command(f):
     wraps(f)
@@ -52,14 +61,16 @@ class FullText(unittest.TestCase):
             return TEST
         fulltext.add('application/test', '.test', test_handler)
         self.assertEqual(fulltext.get('files/test.test'), TEST)
-        self.assertEqual(fulltext.get(file('files/test.test', 'r')), TEST)
+        with open('files/test.test', 'r') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     def test_command(self):
         """Ensures that commands registered for a given type are executed by the `run_command` handler
         when that type is converted."""
         fulltext.add('application/test', '.test', fulltext.run_command, (('echo', TEST), ('echo', TEST), ))
         self.assertEqual(fulltext.get('files/test.test'), TEST)
-        self.assertEqual(fulltext.get(file('files/test.test', 'r')), TEST)
+        with open('files/test.test', 'r') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
 
 class FullTextFiles(unittest.TestCase):
@@ -103,35 +114,68 @@ class FullTextFds(unittest.TestCase):
 
     @allow_missing_command
     def test_odt(self):
-        self.assertEqual(fulltext.get(file('files/test.odt', 'r')), TEST)
+        with open('files/test.odt', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_ods(self):
-        self.assertEqual(fulltext.get(file('files/test.ods', 'r')), TEST)
+        with open('files/test.ods', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_doc(self):
-        self.assertEqual(fulltext.get(file('files/test.doc', 'r')), TEST)
+        with open('files/test.doc', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_pdf(self):
-        self.assertEqual(fulltext.get(file('files/test.pdf', 'r')), TEST)
+        with open('files/test.pdf', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_rtf(self):
-        self.assertEqual(fulltext.get(file('files/test.rtf', 'r')), TEST)
+        with open('files/test.rtf', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_xls(self):
-        self.assertEqual(fulltext.get(file('files/test.xls', 'r')), TEST)
+        with open('files/test.xls', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_txt(self):
-        self.assertEqual(fulltext.get(file('files/test.txt', 'r')), TEST)
+        with open('files/test.txt', 'r') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
 
     @allow_missing_command
     def test_zip(self):
-        self.assertEqual(fulltext.get(file('files/test.zip', 'r')), TEST)
+        with open('files/test.zip', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), TEST)
+
+class FullTextEncodedFiles(unittest.TestCase):
+    "Tests various file types using disk file method."
+
+    @allow_missing_command
+    def test_txt(self):
+        self.assertEqual(fulltext.get('files/test_enc.txt'), ENC_TEST)
+
+    @allow_missing_command
+    def test_doc(self):
+        self.assertEqual(fulltext.get('files/test_enc.doc'), ENC_TEST)
+
+
+class FullTextEncodedFds(unittest.TestCase):
+    "Tests various file types using file-like object method."
+
+    @allow_missing_command
+    def test_txt(self):
+        with open('files/test_enc.txt', 'r') as fo:
+            self.assertEqual(fulltext.get(fo), ENC_TEST)
+
+    @allow_missing_command
+    def test_doc(self):
+        with open('files/test_enc.doc', 'rb') as fo:
+            self.assertEqual(fulltext.get(fo), ENC_TEST)
 
 
 class FullTextCheck(unittest.TestCase):
@@ -141,17 +185,18 @@ class FullTextCheck(unittest.TestCase):
         "At least verify the function executes without an error."
         # The output can be ignored
         stdout = sys.stdout
-        try:
-            sys.stdout = open(os.devnull, 'w')
-        except:
-            # We tried... not core to the test though.
-            pass
-        try:
-            fulltext.check()
-        except Exception as e:
-            self.fail(str(e))
-        finally:
-            sys.stdout = stdout
+        with open(os.devnull, 'w') as stdout_fo:
+            try:
+                sys.stdout = stdout_fo
+            except:
+                # We tried... not core to the test though.
+                pass
+            try:
+                fulltext.check()
+            except Exception as e:
+                self.fail(str(e))
+            finally:
+                sys.stdout = stdout
 
 
 def main():
