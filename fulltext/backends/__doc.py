7f50ff1faa607317dab3ff43818f0c328ab51a8b
Ben Timby <btimby@gmail.com>
2017-09-25 15:56:03 -0400

    Code quality.

--- fulltext/backends/__doc.py
+++ fulltext/backends/__doc.py
@@ -21,6 +21,7 @@ def _get_file(f, **kwargs):
     text = run('antiword', '-', stdin=f).decode('utf8')
     return text
 
+
 def _get_path(path, **kwargs):
     text = run('antiword', path).decode('utf8')
     return text
