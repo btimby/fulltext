4df3f65fe96e5422145aa9ae7dad51eb06853a3b
Ben Timby <btimby@gmail.com>
2017-09-25 13:02:08 -0400

    Define requirements.

--- setup.py
+++ setup.py
@@ -11,19 +11,26 @@ readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 versrel = version + '-' + release
 long_description = file(readme).read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+required = [r for r in required if not r.startswith('git')]
+
+
 setup(
-    name = name,
-    version = versrel,
-    description = 'Convert binary files to plain text for indexing.',
-    long_description = long_description,
-    author = 'Ben Timby',
-    author_email = 'btimby@gmail.com',
-    maintainer = 'Ben Timby',
-    maintainer_email = 'btimby@gmail.com',
-    url = 'http://github.com/btimby/' + name + '/',
-    license = 'GPLv3',
-    packages = find_packages(),
-    classifiers = (
+    name=name,
+    version=versrel,
+    description='Convert binary files to plain text for indexing.',
+    long_description=long_description,
+    author='Ben Timby',
+    author_email='btimby@gmail.com',
+    maintainer='Ben Timby',
+    maintainer_email='btimby@gmail.com',
+    url='http://github.com/btimby/' + name + '/',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=(
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'Operating System :: OS Independent',
