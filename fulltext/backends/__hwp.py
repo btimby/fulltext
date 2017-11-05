9fcece84e81e4b41761fcb6d5f652de111a93d17
Hans Roh <hansroh@gmail.com>
2017-11-05 10:12:06 +0000

    remove generalizing spaces

--- fulltext/backends/__hwp.py
+++ fulltext/backends/__hwp.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 import logging
-import re
 from html2text import config
 import html2text
 from fulltext.util import run, which
@@ -13,19 +12,20 @@ LOGGER.addHandler(logging.NullHandler())
 
 EXTENSIONS = ('hwp',)
 
-STRIP_EOL = re.compile(r'\s+')
 
 if which('hwp5proc') is None:
     LOGGER.warning('CLI tool "hwp5proc" is required for .hwp backend. use "pip install pyhwp"')
+
 
 def _cmd(path, **kwargs):
     cmd = ['hwp5proc', 'xml']
     cmd.extend([path])
     return cmd
 
-def to_text (html):
-    return STRIP_EOL.sub (' ', html2text.html2text(html))
- 
+
+def to_text(html):
+    return html2text.html2text(html)
+
+
 def _get_path(path, **kwargs):
-    return to_text (run(*_cmd(path, **kwargs)).decode('utf-8'))
-    
+    return to_text(run(*_cmd(path, **kwargs)).decode('utf-8'))
