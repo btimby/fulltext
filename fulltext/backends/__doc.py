2161258fbf7833e1b03ba4008c7612c1c532db7f
Ben Timby <btimby@gmail.com>
2017-11-09 15:39:18 -0500

    Add support for older Word docs.

--- fulltext/backends/__doc.py
+++ fulltext/backends/__doc.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import re
 import logging
 
-from fulltext.util import run, which
+from fulltext.util import run, which, ShellError
 
 
 LOGGER = logging.getLogger(__name__)
@@ -11,18 +11,34 @@ LOGGER.addHandler(logging.NullHandler())
 
 EXTENSIONS = ('doc', )
 
-# Antiword wraps our output, any single newline is wrapped, multiple newlines
-# are preserved, and will be folded by fulltext later.
-STRIP_EOL = re.compile(r'\r?\n')
-
-
 if which('antiword') is None:
     LOGGER.warning('CLI tool "antiword" is required for .doc backend.')
 
+if which('abiword') is None:
+    LOGGER.warning('CLI tool "abiword" is optional for .doc backend.')
+
 
 def _get_file(f, **kwargs):
-    return run('antiword', '-', stdin=f).decode('utf8')
+    try:
+        return run('antiword', '-', stdin=f).decode('utf8')
+    except ShellError as e:
+        if not b'not a Word Document' in e.stderr:
+            raise
+
+    f.seek(0)
+
+    # Try abiword, slower, but supports more formats.
+    return run(
+        'abiword', '--to=txt', '--to-name=fd://1', 'fd://0', stdin=f
+    ).decode('utf8')
 
 
 def _get_path(path, **kwargs):
-    return run('antiword', path).decode('utf8')
+    try:
+        return run('antiword', path).decode('utf8')
+    except ShellError as e:
+        if not b'not a Word Document' in e.stderr:
+            raise
+
+    # Try abiword, slower, but supports more formats.
+    return run('abiword', '--to=txt', '--to-name=fd://1', path).decode('utf8')
