7f1517692f1d67b63cd8f25a477baed0655bcbc7
Giampaolo Rodola <g.rodola@gmail.com>
2017-11-30 22:06:01 +0100

    add a compat module; close zip file objs

--- fulltext/backends/__zip.py
+++ fulltext/backends/__zip.py
@@ -3,25 +3,24 @@ from __future__ import absolute_import
 import zipfile
 
 from six import StringIO
+from contextlib2 import ExitStack
 
 from fulltext import get
 
 
 def _get_file(f, **kwargs):
-    text, z = StringIO(), zipfile.ZipFile(f, 'r')
-    for name in sorted(z.namelist()):
-        zf = z.open(name, 'r')
-
-        # Kinda hacky, but zipfile's open() does not handle "b" in the mode.
-        # We do this here to satisy an assertion in _get_file().
-        zf.mode += 'b'
-
-        try:
+    with ExitStack() as stack:
+        text = StringIO()
+        z = stack.enter_context(zipfile.ZipFile(f, 'r'))
+        for name in sorted(z.namelist()):
+            zf = stack.enter_context(z.open(name, 'r'))
+            # Kinda hacky, but zipfile's open() does not handle "b" in
+            # the mode.
+            # We do this here to satisy an assertion in _get_file().
+            zf.mode += 'b'
             text.write(get(zf, name=name))
-        finally:
-            zf.close()
 
-    return text.getvalue()
+        return text.getvalue()
 
 
 _get_path = _get_file
