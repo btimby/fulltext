0eb46c140679b83ca890230f81b01eaa067376d9
Ben Timby <btimby@gmail.com>
2017-11-30 12:40:14 -0500

    Strip out XML declaration/doctype.

--- fulltext/backends/__xml.py
+++ fulltext/backends/__xml.py
@@ -1,14 +1,26 @@
 from __future__ import absolute_import
 
-from bs4 import BeautifulSoup
+import re
+
+import bs4
 
 from six import StringIO
 
 
-def _get_file(f, **kwargs):
-    text, bs = StringIO(), BeautifulSoup(f, 'lxml')
+def _visible(elem):
+    if isinstance(elem, (bs4.element.ProcessingInstruction,
+                         bs4.element.Doctype)):
+        return False
 
-    for elem in bs.findAll(text=True):
+    elif re.match('<!--.*-->', str(elem.encode('utf8'))):
+        return False
+
+    return True
+
+def _get_file(f, **kwargs):
+    text, bs = StringIO(), bs4.BeautifulSoup(f, 'lxml')
+
+    for elem in filter(_visible, bs.findAll(text=True)):
         text.write(elem)
         text.write(u' ')
 
