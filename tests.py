6548f97a147bc42619864a8c52a58e61ce05a47d
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-04 16:56:07 +0100

    add test for json

--- tests.py
+++ tests.py
@@ -632,5 +632,9 @@ class TestUnicodePng(BaseTestCase, TestUnicodeBase):
         pass
 
 
+class TestUnicodeJson(BaseTestCase, TestUnicodeBase):
+    ext = "json"
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
