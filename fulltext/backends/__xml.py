a0cda269d7aff847f35b0870348c6e009048bd40
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-04 22:24:44 +0100

    fix xml backend on py 2

--- fulltext/backends/__xml.py
+++ fulltext/backends/__xml.py
@@ -5,14 +5,17 @@ import re
 import bs4
 
 from six import StringIO
+from six import PY3
 
 
-def _visible(elem):
+def _is_visible(elem, encoding, errors):
     if isinstance(elem, (bs4.element.ProcessingInstruction,
                          bs4.element.Doctype)):
         return False
 
-    elif re.match('<!--.*-->', str(elem)):
+    if not PY3:
+        elem = elem.encode(encoding, errors)
+    if re.match('<!--.*-->', elem):
         return False
 
     return True
@@ -24,8 +27,9 @@ def _get_file(f, **kwargs):
     tdata = bdata.decode(encoding, errors)
     text, bs = StringIO(), bs4.BeautifulSoup(tdata, 'lxml')
 
-    for elem in filter(_visible, bs.findAll(text=True)):
-        text.write(elem)
-        text.write(u' ')
+    for elem in bs.findAll(text=True):
+        if _is_visible(elem, encoding, errors):
+            text.write(elem)
+            text.write(u' ')
 
     return text.getvalue()
