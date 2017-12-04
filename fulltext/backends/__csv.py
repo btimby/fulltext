4801a64194a3e349bccb950c891427a3a6612d55
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-04 22:16:39 +0100

    make csv backend work with python 2

--- fulltext/backends/__csv.py
+++ fulltext/backends/__csv.py
@@ -3,11 +3,20 @@ from __future__ import absolute_import
 import csv
 
 from six import StringIO
+from six import PY3
 
 
-def readlines(f, encoding, errors):
-    for line in f.readlines():
-        yield line.decode(encoding, errors)
+if PY3:
+    def readlines(f, encoding, errors):
+        for line in f.readlines():
+            yield line.decode(encoding, errors)
+
+else:
+    def unicode_reader(f, encoding, encoding_errors, **opts):
+        reader = csv.reader(f, **opts)
+        for row in reader:
+            yield [unicode(cell, encoding, encoding_errors)  # NOQA
+                   for cell in row]
 
 
 def _get_file(f, **kwargs):
@@ -25,9 +34,14 @@ def _get_file(f, **kwargs):
     elif mimetype == 'text/psv':
         options['delimiter'] = '|'
 
+    if PY3:
+        gen = readlines(f, encoding, errors)
+        reader = csv.reader(gen, **options)
+    else:
+        reader = unicode_reader(f, encoding, errors, **options)
+
     text = StringIO()
-    gen = readlines(f, encoding, errors)
-    for row in csv.reader(gen, **options):
+    for row in reader:
         text.write(u' '.join(row))
         text.write(u'\n')
 
