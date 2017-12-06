afab9e15e192c9a897c2106c6e0d89c903588fa2
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-06 14:28:37 +0100

    provide a test sub-cmd

--- fulltext/__main__.py
+++ fulltext/__main__.py
@@ -1,9 +1,7 @@
-"""
-Fulltext CLI interface.
-"""
 
 from __future__ import absolute_import
 
+import os
 import sys
 import logging
 
@@ -12,17 +10,35 @@ from docopt import docopt
 import fulltext
 
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
 def _handle_open(path):
     with open(path, 'rb') as f:
         return fulltext.get(f)
 
 
+def test_modules():
+    path = os.path.join(HERE, "backends")
+    for name in os.listdir(path):
+        if not name.endswith('.py'):
+            continue
+        mod_name = "fulltext.backends.%s" % (
+            os.path.splitext(os.path.basename(name))[0])
+        mod = __import__(mod_name, fromlist=[' '])
+        print(mod)
+
+
 def main(args=sys.argv[1:]):
-    """
-    Extract text from a file.
+    """Extract text from a file.
+
+    Commands:
+        extract - extract text from path
+        test    - make sure all deps are installed
 
     Usage:
-        fulltext [-f] <path>...
+        fulltext extract [-f] [-t] <path>...
+        fulltext test
 
     Options:
         -f      Open file first.
@@ -32,13 +48,16 @@ def main(args=sys.argv[1:]):
     logger = logging.getLogger()
     logger.addHandler(logging.StreamHandler())
 
-    handler = fulltext.get
+    if opt['test']:
+        test_modules()
+    else:
+        handler = fulltext.get
 
-    if opt['-f']:
-        handler = _handle_open
+        if opt['-f']:
+            handler = _handle_open
 
-    for path in opt['<path>']:
-        print(handler(path))
+        for path in opt['<path>']:
+            print(handler(path))
 
 
 if __name__ == '__main__':
