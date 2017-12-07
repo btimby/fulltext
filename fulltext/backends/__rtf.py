0e9dcc8120c5b5b2b5ba5762d282c61c1bc7c1f8
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-07 15:37:14 +0100

    port rtf

--- fulltext/backends/__rtf.py
+++ fulltext/backends/__rtf.py
@@ -1,23 +1,24 @@
 from __future__ import absolute_import
 from fulltext.util import run
 from fulltext.util import assert_cmd_exists
-
-
-def check():
-    assert_cmd_exists('unrtf')
+from fulltext import BaseBackend
 
 
 def strip(text, encoding, errors):
     return text.partition(b'-----------------')[2].decode(encoding, errors)
 
 
-def handle_fobj(f, **kwargs):
-    encoding, errors = kwargs['encoding'], kwargs['encoding_errors']
-    return strip(run('unrtf', '--text', '--nopict', stdin=f),
-                 encoding, errors)
+class Backend(BaseBackend):
 
+    def check(self):
+        assert_cmd_exists('unrtf')
 
-def handle_path(path, **kwargs):
-    encoding, errors = kwargs['encoding'], kwargs['encoding_errors']
-    return strip(run('unrtf', '--text', '--nopict', path),
-                 encoding, errors)
+    def handle_fobj(self, f):
+        encoding, errors = self.encoding, self.encoding_errors
+        return strip(run('unrtf', '--text', '--nopict', stdin=f),
+                     encoding, errors)
+
+    def handle_path(self, path):
+        encoding, errors = self.encoding, self.encoding_errors
+        return strip(run('unrtf', '--text', '--nopict', path),
+                     encoding, errors)
