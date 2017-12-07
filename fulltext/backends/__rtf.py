f54eb1b88668fdab1ac105d9c7f61451cc5f8c93
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-07 15:38:15 +0100

    port rtf

--- fulltext/backends/__rtf.py
+++ fulltext/backends/__rtf.py
@@ -4,21 +4,18 @@ from fulltext.util import assert_cmd_exists
 from fulltext import BaseBackend
 
 
-def strip(text, encoding, errors):
-    return text.partition(b'-----------------')[2].decode(encoding, errors)
-
-
 class Backend(BaseBackend):
 
     def check(self):
         assert_cmd_exists('unrtf')
 
+    def strip(self, text):
+        return self.decode(text.partition(b'-----------------')[2])
+
     def handle_fobj(self, f):
-        encoding, errors = self.encoding, self.encoding_errors
-        return strip(run('unrtf', '--text', '--nopict', stdin=f),
-                     encoding, errors)
+        return self.strip(
+            run('unrtf', '--text', '--nopict', stdin=f))
 
     def handle_path(self, path):
-        encoding, errors = self.encoding, self.encoding_errors
-        return strip(run('unrtf', '--text', '--nopict', path),
-                     encoding, errors)
+        return self.strip(
+            run('unrtf', '--text', '--nopict', path))
