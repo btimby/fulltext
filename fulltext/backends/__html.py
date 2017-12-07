61c5c4ec836701ede0bf6a19aee4507bf1621cc1
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-07 15:39:57 +0100

    port html

--- fulltext/backends/__html.py
+++ fulltext/backends/__html.py
@@ -7,28 +7,30 @@ from bs4 import BeautifulSoup
 from six import StringIO
 from six import PY3
 
-
-def is_visible(elem, encoding, errors):
-    if elem.parent.name in ['style', 'script', '[document]', 'head']:
-        return False
-
-    if not PY3:
-        elem = elem.encode(encoding, errors)
-    if re.match('<!--.*-->', elem):
-        return False
-
-    return True
+from fulltext import BaseBackend
 
 
-def handle_fobj(f, **kwargs):
-    encoding, errors = kwargs['encoding'], kwargs['encoding_errors']
-    data = f.read()
-    data = data.decode(encoding, errors)
-    text, bs = StringIO(), BeautifulSoup(data, 'lxml')
+class Backend(BaseBackend):
 
-    for elem in bs.findAll(text=True):
-        if is_visible(elem, encoding, errors):
-            text.write(elem)
-            text.write(u' ')
+    def is_visible(self, elem):
+        if elem.parent.name in ['style', 'script', '[document]', 'head']:
+            return False
 
-    return text.getvalue()
+        if not PY3:
+            elem = elem.encode(self.encoding, self.encoding_errors)
+        if re.match('<!--.*-->', elem):
+            return False
+
+        return True
+
+    def handle_fobj(self, f):
+        data = f.read()
+        data = self.decode(data)
+        text, bs = StringIO(), BeautifulSoup(data, 'lxml')
+
+        for elem in bs.findAll(text=True):
+            if self.is_visible(elem):
+                text.write(elem)
+                text.write(u' ')
+
+        return text.getvalue()
