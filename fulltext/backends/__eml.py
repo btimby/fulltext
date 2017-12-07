f8ba731d04b88f83871530f27904e98bd2ccd4cd
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-07 15:07:08 +0100

    do mbox and eml

--- fulltext/backends/__eml.py
+++ fulltext/backends/__eml.py
@@ -3,10 +3,10 @@ from email import message_from_file
 import codecs
 
 from six import StringIO
+from fulltext import BaseBackend
 
 
-def handle_fobj(f, **kwargs):
-    encoding, errors = kwargs['encoding'], kwargs['encoding_errors']
+def handle_fobj(f, encoding, errors):
     text = StringIO()
     f2 = codecs.getreader(encoding)(f, errors=errors)
     m = message_from_file(f2)
@@ -16,3 +16,9 @@ def handle_fobj(f, **kwargs):
             text.write(part.get_payload())
 
     return text.getvalue()
+
+
+class Backend(BaseBackend):
+
+    def handle_fobj(self, f):
+        return handle_fobj(f, self.encoding, self.encoding_errors)
