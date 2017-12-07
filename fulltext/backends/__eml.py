e7fb20123d0e774d2f261619a25ac30861e7d9c8
Giampaolo Rodola <g.rodola@gmail.com>
2017-12-07 20:44:45 +0100

    rename var

--- fulltext/backends/__eml.py
+++ fulltext/backends/__eml.py
@@ -9,8 +9,8 @@ from fulltext import BaseBackend
 # This is used by other modules, hence it's here.
 def handle_fobj(f, encoding, errors):
     text = StringIO()
-    f2 = codecs.getreader(encoding)(f, errors=errors)
-    m = message_from_file(f2)
+    encf = codecs.getreader(encoding)(f, errors=errors)
+    m = message_from_file(encf)
 
     for part in m.walk():
         if part.get_content_type().startswith('text/plain'):
